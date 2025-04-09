c455071de614345ef748459dd5deb4c58269e866
yashksaini-coder <ys3853428@gmail.com>
2025-04-09 22:37:43 +0530

    Fix checkUser auth

--- components/header.jsx
+++ components/header.jsx
@@ -27,7 +27,7 @@ export default async function Header() {
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
-            
+            src="/logo.png"
             alt="Sensai Logo"
             width={200}
             height={60}
